423c5806b6881d3b2c78c2291b93dd0425ed6199
Bruno Spolidorio <b.spolidorio@gmail.com>
2020-07-07 14:50:52 +0100

    Fix url

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,11 +2,11 @@ import { Request, Response } from "express";
 
 const user = [
   {
-    name: "Bruno",
+    name: "Bruno Spolidorio",
     email: "bspolidorio@gmail.com",
     phone: "936 019 281",
-    github: "http://github.com/bspolidorio",
-    linkedin: "https://www.linkedin.com/in/bspolidorio/",
+    github: "https://github.com/bspolidorio",
+    linkedin: "https://pt.linkedin.com/in/bspolidorio/",
     about:
       'From the instant I produced "Hello World" in the console of my first application, I knew I was hooked into the world of software development. But software development has never been "just a job" for me, it\'s offered an engaging challenge to continually learn and improve my skills in creating high-quality software. What started with a simple "Hello World" has become a full-fledged passion that only gets more exciting as the time go by.',
     location: "Porto - Portugal",
@@ -27,7 +27,7 @@ const user = [
         name: "Cantagalli Corretora",
         description:
           "This was my first real world website, this page was designed inraw HTML and CSS to copy the original design of a template inWordPress following the client expetation.",
-        code: "http://github.com/bspolidorio/cantagalli_seguradora",
+        code: "https://github.com/bspolidorio/cantagalli_seguradora",
         live: "http://cantagalli.com.br/",
       },
       {
@@ -35,7 +35,7 @@ const user = [
         description:
           "A Star Wars fan page, created in React.js and Apollo-client tointeract with a GraphQL API. Styled using Styled-components.",
         code: "https://github.com/bspolidorio/starwars",
-        live: "http://starwars-react.herokuapp.com/",
+        live: "https://starwars-react.herokuapp.com/",
       },
       {
         name: "Task Counter",
